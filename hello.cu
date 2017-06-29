139be32680479565ad25749ea4df781af3e2db57
Mathias Wagner <mathiasw@nvidia.com>
2017-06-29 15:57:39 +0200

    modified kernel

--- hello.cu
+++ hello.cu
@@ -4,7 +4,7 @@ __global__
 void saxpy(int n, float a, float *x, float *y)
 {
   int i = blockIdx.x*blockDim.x + threadIdx.x;
-  if (i < n) y[i] = a*x[i] + y[i];
+  if (i < n) y[i] = 3.f*a*x[i] + y[i];
 }
 
 int main(void)
